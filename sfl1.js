abf25cc8708150fa26163225382e64546667f647
tengik <40707000+tengik@users.noreply.github.com>
2018-07-02 12:13:38 +0700

    Add files via upload

--- sfl1.js
+++ sfl1.js
@@ -1,50 +1,27 @@
-<div style="border-radius: 10px; border: 1px solid #e4e4e4; padding: 10px;">
-<div class="row margin-bottom-20">
-<div class="col-md-4 text-center">
-<h3>
-Advertise</h3>
-Kode Iklan Disini
-</div>
-<div class="col-md-4 margin-top-25">
-<div class="panel panel-success text-center">
-<div class="panel-heading">
-<h2>
-<i class="fa fa-shield"></i> SafeLink Converter <i class="fa fa-shield"></i></h2>
-</div>
-<div class="panel-body text-center">
-<div class="text-primary margin-bottom-20">
-Encrypting your link and protect the link from viruses, malware, thief, etc!<br />
-Made your link safe to visit.
-</div>
-<div class="progress" id="daplong">
-<div class="progress-bar progress-bar-striped active six-sec-ease-in-out" data-transitiongoal="100" role="progressbar">
-</div>
-</div>
-<button class="alert alert-dismissible alert-success btn-lg" id="download2" style="display: none;"><i aria-hidden="true" class="fa fa-thumbs-o-up"></i> <strong>Well done!</strong> you have successfully gained access to Decrypted Link. <i aria-hidden="true" class="fa fa-hand-o-down slideInDownThumb"></i></button>
-<br />
-<div class="panel-body">
-<script src="https://cdn.rawgit.com/mulyanacymot/safelinkliknew/2d2fb103/safelinknew.js" type="text/javascript"></script>
-<script type="text/javascript">
-var currentURL=location.href;
-var str = currentURL;
-var res = str.replace("https://safelinklik.blogspot.com/p/understanding-downloads-how-it-works.html?url=", "");
-document.write('<button type="button" id="download" class="Visit_Link btn btn-success" onclick="changeLink();" style="display: none;"><strong>Visit Link</strong> <i class="fa fa-external-link" aria-hidden="true"></i></button>')
-</script>
-  </div>
-</div>
-</div>
-</div>
-<div class="col-md-4 text-center">
-<h3>
-Advertise</h3>
-Kode Iklan Disini
-</div>
-<div class="clear">
-</div>
-</div>
-</div>
-<div class="container">
-<div class="text-left">
-</div>
-</div>
-<span style="background-color: white; color: #333333; font-family: &quot;helvetica neue&quot; , &quot;helvetica&quot; , &quot;arial&quot; , sans-serif; font-size: 14px;">Download is one of the many activities done by the users of the Internet network. For people who often open the virtual world / internet, the term download is certainly not a foreign term for their ears. But for people who are rarely connected to the internet world, the term download is certainly not a familiar term for their ears. Some lay people have not even heard or used the term download at all.</span><span style="background-color: white; color: #333333; font-family: &quot;helvetica neue&quot; , &quot;helvetica&quot; , &quot;arial&quot; , sans-serif; font-size: 14px;">Download is one of the many activities done by the users of the Internet network. For people who often open the virtual world / internet, the term download is certainly not a foreign term for their ears. But for people who are rarely connected to the internet world, the term download is certainly not a familiar term for their ears. Some lay people have not even heard or used the term download at all.</span><span style="background-color: white; color: #333333; font-family: &quot;helvetica neue&quot; , &quot;helvetica&quot; , &quot;arial&quot; , sans-serif; font-size: 14px;">Download is one of the many activities done by the users of the Internet network. For people who often open the virtual world / internet, the term download is certainly not a foreign term for their ears. But for people who are rarely connected to the internet world, the term download is certainly not a familiar term for their ears. Some lay people have not even heard or used the term download at all.</span><span style="background-color: white; color: #333333; font-family: &quot;helvetica neue&quot; , &quot;helvetica&quot; , &quot;arial&quot; , sans-serif; font-size: 14px;">Download is one of the many activities done by the users of the Internet network. For people who often open the virtual world / internet, the term download is certainly not a foreign term for their ears. But for people who are rarely connected to the internet world, the term download is certainly not a familiar term for their ears. Some lay people have not even heard or used the term download at all.</span><span style="background-color: white; color: #333333; font-family: &quot;helvetica neue&quot; , &quot;helvetica&quot; , &quot;arial&quot; , sans-serif; font-size: 14px;">Download is one of the many activities done by the users of the Internet network. For people who often open the virtual world / internet, the term download is certainly not a foreign term for their ears. But for people who are rarely connected to the internet world, the term download is certainly not a familiar term for their ears. Some lay people have not even heard or used the term download at all.</span><span style="background-color: white; color: #333333; font-family: &quot;helvetica neue&quot; , &quot;helvetica&quot; , &quot;arial&quot; , sans-serif; font-size: 14px;">Download is one of the many activities done by the users of the Internet network. For people who often open the virtual world / internet, the term download is certainly not a foreign term for their ears. But for people who are rarely connected to the internet world, the term download is certainly not a familiar term for their ears. Some lay people have not even heard or used the term download at all.</span><span style="background-color: white; color: #333333; font-family: &quot;helvetica neue&quot; , &quot;helvetica&quot; , &quot;arial&quot; , sans-serif; font-size: 14px;">Download is one of the many activities done by the users of the Internet network. For people who often open the virtual world / internet, the term download is certainly not a foreign term for their ears. But for people who are rarely connected to the internet world, the term download is certainly not a familiar term for their ears. Some lay people have not even heard or used the term download at all.</span>
+var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(input){var output="";var chr1,chr2,chr3,enc1,enc2,enc3,enc4;var i=0;input=Base64._utf8_encode(input);while(i<input.length){chr1=input.charCodeAt(i++);chr2=input.charCodeAt(i++);chr3=input.charCodeAt(i++);enc1=chr1>>2;enc2=((chr1&3)<<4)|(chr2>>4);enc3=((chr2&15)<<2)|(chr3>>6);enc4=chr3&63;if(isNaN(chr2)){enc3=enc4=64;}else if(isNaN(chr3)){enc4=64;}
+output=output+ this._keyStr.charAt(enc1)+ this._keyStr.charAt(enc2)+ this._keyStr.charAt(enc3)+ this._keyStr.charAt(enc4);}
+return output;},decode:function(input){var output="";var chr1,chr2,chr3;var enc1,enc2,enc3,enc4;var i=0;input=input.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(i<input.length){enc1=this._keyStr.indexOf(input.charAt(i++));enc2=this._keyStr.indexOf(input.charAt(i++));enc3=this._keyStr.indexOf(input.charAt(i++));enc4=this._keyStr.indexOf(input.charAt(i++));chr1=(enc1<<2)|(enc2>>4);chr2=((enc2&15)<<4)|(enc3>>2);chr3=((enc3&3)<<6)|enc4;output=output+ String.fromCharCode(chr1);if(enc3!=64){output=output+ String.fromCharCode(chr2);}
+if(enc4!=64){output=output+ String.fromCharCode(chr3);}}
+output=Base64._utf8_decode(output);return output;},_utf8_encode:function(string){string=string.replace(/\r\n/g,"\n");var utftext="";for(var n=0;n<string.length;n++){var c=string.charCodeAt(n);if(c<128){utftext+=String.fromCharCode(c);}
+else if((c>127)&&(c<2048)){utftext+=String.fromCharCode((c>>6)|192);utftext+=String.fromCharCode((c&63)|128);}
+else{utftext+=String.fromCharCode((c>>12)|224);utftext+=String.fromCharCode(((c>>6)&63)|128);utftext+=String.fromCharCode((c&63)|128);}}
+return utftext;},_utf8_decode:function(utftext){var string="";var i=0;var c=c1=c2=0;while(i<utftext.length){c=utftext.charCodeAt(i);if(c<128){string+=String.fromCharCode(c);i++;}
+else if((c>191)&&(c<224)){c2=utftext.charCodeAt(i+ 1);string+=String.fromCharCode(((c&31)<<6)|(c2&63));i+=2;}
+else{c2=utftext.charCodeAt(i+ 1);c3=utftext.charCodeAt(i+ 2);string+=String.fromCharCode(((c&15)<<12)|((c2&63)<<6)|(c3&63));i+=3;}}
+return string;}}
+var encode=document.getElementById('encode'),decode=document.getElementById('decode'),output=document.getElementById('output'),input=document.getElementById('input');var User_ID="";var protected_links="";var a_to_va=0;var a_to_vb=0;var a_to_vc="";function auto_safelink(){auto_safeconvert();}
+function auto_safeconvert(){var a_to_vd=window.location.hostname;if(protected_links!=""&&!protected_links.match(a_to_vd)){protected_links+=", "+ a_to_vd;}else if(protected_links=="")
+{protected_links=a_to_vd;}
+var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElementsByTagName("a");a_to_va=a_to_ve.length;a_to_vf=a_to_fa();a_to_vg=a_to_vf.length;var a_to_vh=false;var j=0;var a_to_vi="";for(var i=0;i<a_to_va;i++)
+{a_to_vh=false;j=0;while(a_to_vh==false&&j<a_to_vg)
+{a_to_vi=a_to_ve[i].href;if(a_to_vi.match(a_to_vf[j])||!a_to_vi||!a_to_vi.match("http"))
+{a_to_vh=true;}
+j++;}
+if(a_to_vh==false)
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://tersafekan.blogspot.com/p/electronic-music-is-music-that-employs.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
+{a_to_vj.innerHTML+=a_to_vb;}
+if(a_to_vk)
+{a_to_vk.innerHTML+=a_to_va;}}
+function a_to_fa()
+{var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
