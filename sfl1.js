959b1aa333ccd23e3fa159b68dae93206c3b2a24
tengik <40707000+tengik@users.noreply.github.com>
2018-07-02 12:01:04 +0700

    Add files via upload

---
+++ sfl1.js
@@ -0,0 +1,50 @@
+<div style="border-radius: 10px; border: 1px solid #e4e4e4; padding: 10px;">
+<div class="row margin-bottom-20">
+<div class="col-md-4 text-center">
+<h3>
+Advertise</h3>
+Kode Iklan Disini
+</div>
+<div class="col-md-4 margin-top-25">
+<div class="panel panel-success text-center">
+<div class="panel-heading">
+<h2>
+<i class="fa fa-shield"></i> SafeLink Converter <i class="fa fa-shield"></i></h2>
+</div>
+<div class="panel-body text-center">
+<div class="text-primary margin-bottom-20">
+Encrypting your link and protect the link from viruses, malware, thief, etc!<br />
+Made your link safe to visit.
+</div>
+<div class="progress" id="daplong">
+<div class="progress-bar progress-bar-striped active six-sec-ease-in-out" data-transitiongoal="100" role="progressbar">
+</div>
+</div>
+<button class="alert alert-dismissible alert-success btn-lg" id="download2" style="display: none;"><i aria-hidden="true" class="fa fa-thumbs-o-up"></i> <strong>Well done!</strong> you have successfully gained access to Decrypted Link. <i aria-hidden="true" class="fa fa-hand-o-down slideInDownThumb"></i></button>
+<br />
+<div class="panel-body">
+<script src="https://cdn.rawgit.com/mulyanacymot/safelinkliknew/2d2fb103/safelinknew.js" type="text/javascript"></script>
+<script type="text/javascript">
+var currentURL=location.href;
+var str = currentURL;
+var res = str.replace("https://safelinklik.blogspot.com/p/understanding-downloads-how-it-works.html?url=", "");
+document.write('<button type="button" id="download" class="Visit_Link btn btn-success" onclick="changeLink();" style="display: none;"><strong>Visit Link</strong> <i class="fa fa-external-link" aria-hidden="true"></i></button>')
+</script>
+  </div>
+</div>
+</div>
+</div>
+<div class="col-md-4 text-center">
+<h3>
+Advertise</h3>
+Kode Iklan Disini
+</div>
+<div class="clear">
+</div>
+</div>
+</div>
+<div class="container">
+<div class="text-left">
+</div>
+</div>
+<span style="background-color: white; color: #333333; font-family: &quot;helvetica neue&quot; , &quot;helvetica&quot; , &quot;arial&quot; , sans-serif; font-size: 14px;">Download is one of the many activities done by the users of the Internet network. For people who often open the virtual world / internet, the term download is certainly not a foreign term for their ears. But for people who are rarely connected to the internet world, the term download is certainly not a familiar term for their ears. Some lay people have not even heard or used the term download at all.</span><span style="background-color: white; color: #333333; font-family: &quot;helvetica neue&quot; , &quot;helvetica&quot; , &quot;arial&quot; , sans-serif; font-size: 14px;">Download is one of the many activities done by the users of the Internet network. For people who often open the virtual world / internet, the term download is certainly not a foreign term for their ears. But for people who are rarely connected to the internet world, the term download is certainly not a familiar term for their ears. Some lay people have not even heard or used the term download at all.</span><span style="background-color: white; color: #333333; font-family: &quot;helvetica neue&quot; , &quot;helvetica&quot; , &quot;arial&quot; , sans-serif; font-size: 14px;">Download is one of the many activities done by the users of the Internet network. For people who often open the virtual world / internet, the term download is certainly not a foreign term for their ears. But for people who are rarely connected to the internet world, the term download is certainly not a familiar term for their ears. Some lay people have not even heard or used the term download at all.</span><span style="background-color: white; color: #333333; font-family: &quot;helvetica neue&quot; , &quot;helvetica&quot; , &quot;arial&quot; , sans-serif; font-size: 14px;">Download is one of the many activities done by the users of the Internet network. For people who often open the virtual world / internet, the term download is certainly not a foreign term for their ears. But for people who are rarely connected to the internet world, the term download is certainly not a familiar term for their ears. Some lay people have not even heard or used the term download at all.</span><span style="background-color: white; color: #333333; font-family: &quot;helvetica neue&quot; , &quot;helvetica&quot; , &quot;arial&quot; , sans-serif; font-size: 14px;">Download is one of the many activities done by the users of the Internet network. For people who often open the virtual world / internet, the term download is certainly not a foreign term for their ears. But for people who are rarely connected to the internet world, the term download is certainly not a familiar term for their ears. Some lay people have not even heard or used the term download at all.</span><span style="background-color: white; color: #333333; font-family: &quot;helvetica neue&quot; , &quot;helvetica&quot; , &quot;arial&quot; , sans-serif; font-size: 14px;">Download is one of the many activities done by the users of the Internet network. For people who often open the virtual world / internet, the term download is certainly not a foreign term for their ears. But for people who are rarely connected to the internet world, the term download is certainly not a familiar term for their ears. Some lay people have not even heard or used the term download at all.</span><span style="background-color: white; color: #333333; font-family: &quot;helvetica neue&quot; , &quot;helvetica&quot; , &quot;arial&quot; , sans-serif; font-size: 14px;">Download is one of the many activities done by the users of the Internet network. For people who often open the virtual world / internet, the term download is certainly not a foreign term for their ears. But for people who are rarely connected to the internet world, the term download is certainly not a familiar term for their ears. Some lay people have not even heard or used the term download at all.</span>
